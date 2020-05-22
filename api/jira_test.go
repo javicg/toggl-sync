c3a1a7e96289cfc07672586d88fe9f4582bfe54a
javi.carretero <javi.carretero@trendminer.com>
2020-05-22 11:52:30 +0200

    Revert "Force test error to check output on GitHub Workflow"
    This reverts commit d50edf3a

--- api/jira_test.go
+++ api/jira_test.go
@@ -13,7 +13,7 @@ func TestJiraApi_LogWork(t *testing.T) {
 	ticket := "EXAMPLE-1234"
 	expectedEntry := WorkLogEntry{
 		Comment:          "Added automatically by toggl-sync",
-		TimeSpentSeconds: 61,
+		TimeSpentSeconds: 60,
 	}
 
 	server := NewHttpServer().
